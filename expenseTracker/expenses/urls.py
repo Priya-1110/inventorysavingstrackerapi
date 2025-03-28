d4c1384e0d732aa0f0e257d8cf49813d1859e602
EC2 Default User <ec2-user@ip-172-31-1-99.eu-west-1.compute.internal>
2025-03-28 05:48:54 +0000

    deploy check

--- expenseTracker/expenses/urls.py
+++ expenseTracker/expenses/urls.py
@@ -1,7 +1,6 @@
 from django.urls import path
 from .views import (
     savings_goal_view,
-    home_redirect,
     expense_analyzer_view,
     register_view,
     homepage_view,
@@ -12,15 +11,16 @@ from .views import (
 from django.contrib.auth import views as auth_views
 
 urlpatterns = [
-    path('', home_redirect),
+    path('', register_view, name='register'),  # 👈 This makes register the default homepage
     path('home/', homepage_view, name='home'),
     path('savings-goal/', savings_goal_view, name='savings_goal'),
     path('analyze-expenses/', expense_analyzer_view, name='analyze_expenses'),
     path('dashboard/', user_dashboard_view, name='dashboard'),
     path('delete-entry/', delete_entry_view, name='delete_entry'),
     path("ai-tip-entry/", generate_ai_tip_entry, name="generate_ai_tip_entry"),
+
     # 🧑‍💻 User Auth
-    path('register/', register_view, name='register'),
+    path('register/', register_view),
     path('login/', auth_views.LoginView.as_view(template_name='login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(next_page='login'), name='logout'),
 ]
